a1008c1e106972bbc4837ad0f123d30739151b2c
Alvaro Cofre <alvaro.cofre@aimmanager.com>
2022-05-06 17:59:19 -0400

    Fix root directory

---
+++ Sources/SwiftUIBestPractices/Extensions/ExtensionView.swift
@@ -0,0 +1,45 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Alvaro Cofre Perez on 06-05-22.
+//
+
+import SwiftUI
+
+extension View {
+    func hLeading() -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    func hTrailing() -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+    func hCenter() -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: .center)
+    }
+}
+
+struct ExtensionView: View {
+    var body: some View {
+        VStack {
+            Circle()
+                .frame(width: 50, height: 50)
+                .hLeading()
+            Circle()
+                .frame(width: 50, height: 50)
+                .hCenter()
+            Circle()
+                .frame(width: 50, height: 50)
+                .hTrailing()
+        }
+    }
+}
+
+struct ExtensionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtensionView()
+    }
+}
